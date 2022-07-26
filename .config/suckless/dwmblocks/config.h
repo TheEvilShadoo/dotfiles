ce7bb6cbc1eaf55ccac0d4a7381c86e51e540a4c
TheEvilShadoo <ryan-skis-18@outlook.com>
2022-07-26 15:20:13 -0400

    Fix spacing

--- .config/suckless/dwmblocks/config.h
+++ .config/suckless/dwmblocks/config.h
@@ -3,13 +3,13 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
     {"",    "cat /tmp/recordingicon 2>/dev/null",	0,	9},
     {"",    "sb-kernel",    0,   2},
-    {"",	"sb-cpu",   	1,	18},
-    {"",	"sb-memory",	1,	14},
-    {"",	"sb-disk",  	1,	12},
-    {"",	"sb-nettraf",	1,	16},
+    {"",    "sb-cpu",       1,  18},
+    {"",    "sb-memory",    1,  14},
+    {"",    "sb-disk",      1,  12},
+    {"",    "sb-nettraf",   1,  16},
     {"",    "sb-volume",    1,  10},
     {"",    "sb-music",     0,  11},
-    {"",	"sb-clock",     1,	 1},
+    {"",    "sb-clock",     1,   1},
     {"",    "sb-doppler",   0,  19},
 };
 
