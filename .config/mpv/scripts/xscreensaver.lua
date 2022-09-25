6215c9d33f669aa94857159042d7c88804587db8
TheEvilShadoo <ryan-skis-18@outlook.com>
2022-09-25 19:47:24 -0400

    Update dotfiles

--- .config/mpv/scripts/xscreensaver.lua
+++ .config/mpv/scripts/xscreensaver.lua
@@ -1,24 +1,27 @@
 -- this script periodically deactivates xscreensaver
 -- when video playback is active
 
-local function heartbeat()
-    if mp.get_property_native("pause") or
-       mp.get_property_native("idle")  or
-       not mp.get_property_native("vo-configured") then
-        return
+if os.getenv("TERM") == "linux" then
+else
+    local function heartbeat()
+        if mp.get_property_native("pause") or
+           mp.get_property_native("idle")  or
+           not mp.get_property_native("vo-configured") then
+            return
+        end
+
+        mp.command_native_async(
+            {
+              name           = "subprocess",
+              args           = { "xscreensaver-command", "-deactivate" },
+              capture_stdout = true,
+            },
+            function () end)
     end
 
-    mp.command_native_async(
-        {
-          name           = "subprocess",
-          args           = { "xscreensaver-command", "-deactivate" },
-          capture_stdout = true,
-        },
-        function () end)
-end
+    mp.add_periodic_timer(60, heartbeat)
 
-mp.add_periodic_timer(60, heartbeat)
-
-for _, prop in ipairs({"pause", "idle", "vo-configured"}) do
-    mp.observe_property(prop, nil, heartbeat)
+    for _, prop in ipairs({"pause", "idle", "vo-configured"}) do
+        mp.observe_property(prop, nil, heartbeat)
+    end
 end
