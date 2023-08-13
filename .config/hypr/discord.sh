9de514df62a54a0b2c7ba418b94480bb68a94390
TheEvilShadoo <ryan-skis-18@outlook.com>
2023-08-13 10:04:48 -0400

    Update dotfiles

--- .config/hypr/discord.sh
+++ .config/hypr/discord.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-DEST=`busctl --user | grep Discord | grep -o ":1\.[0-9]*" | uniq`
-for val in $DEST; do
-    dbus-send --dest=$val --type=method_call /StatusNotifierItem org.kde.StatusNotifierItem.Activate
-done
+if pidof "Discord"; then
+    DEST=`busctl --user | grep Discord | grep -o ":1\.[0-9]*" | uniq`
+    for val in $DEST; do
+        dbus-send --dest=$val --type=method_call /StatusNotifierItem org.kde.StatusNotifierItem.Activate
+    done
+else
+    discord --enable-features=UseOzonePlatform --ozone-platform=wayland --start-minimized &
+fi
+
 hyprctl dispatch workspace 5
