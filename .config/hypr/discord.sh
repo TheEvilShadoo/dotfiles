054ebc3d8068a7a29c703293ea78dc550d3a699b
TheEvilShadoo <ryan-skis-18@outlook.com>
2024-06-27 15:36:54 -0400

    Update dotfiles

--- .config/hypr/discord.sh
+++ .config/hypr/discord.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-if pidof "vesktop.bin"; then
-    DEST=`busctl --user | grep vesktop.bin | grep -o ":1\.[0-9]*" | uniq`
+if pidof "vesktop"; then
+    DEST=`busctl --user | grep vesktop | grep -o ":1\.[0-9]*" | uniq`
     for val in $DEST; do
         dbus-send --dest=$val --type=method_call /StatusNotifierItem org.kde.StatusNotifierItem.Activate
     done
 else
-    /usr/local/bin/Vesktop.AppImage --enable-blink-features=MiddleClickAutoscroll --start-minimized &
+    vesktop --enable-blink-features=MiddleClickAutoscroll --start-minimized &
 fi
